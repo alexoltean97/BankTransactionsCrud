c742c258b84575cd013e2ba1eceef459295224f4
alexoltean97 <alex.oltean97@gmail.com>
2023-01-05 11:26:18 +0200

    Initial modals setup, migrations and Controllers

---
+++ OnlineLibrary/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineLibrary.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
